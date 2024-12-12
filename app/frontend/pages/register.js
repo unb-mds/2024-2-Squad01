0dca5f37cfe91bd7414bab20daf008a26e8a3c7a
Maria Clara Sena <mclarasenalimaa@gmail.com>
2024-12-12 15:14:52 -0300

    [style](Login e Cadastro): Corrigindo bug visual do cadastro

--- app/frontend/pages/register.js
+++ app/frontend/pages/register.js
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 import LoginCard from "../components/LoginCard/logincard";
 import Input from "../components/input/input";
 import Button from "../components/button/button";
-import styles from "../style/login.module.css";
+import styles from "../styles/login.module.css";
 
 export default function RegisterPage() {
     const [formData, setFormData] = useState({
