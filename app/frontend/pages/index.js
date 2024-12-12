4bf8050ce92bb550fba4be27014d79d63de1aaca
Maria Clara Sena <mclarasenalimaa@gmail.com>
2024-12-12 17:27:33 -0300

    [style](Página Inicial): Estilização Básica

--- app/frontend/pages/index.js
+++ app/frontend/pages/index.js
@@ -1,19 +1,18 @@
 import Link from 'next/link';
+import LoginCard from "../components/LoginCard/logincard";
+import Button from "../components/button/button";
+import styles from "../styles/login.module.css";
 
 export default function Home() {
     return (
-        <div className="container">
-            <h1>Bem-vindo ao Unbookinho</h1>
-            <nav>
-                <ul className="navList">
-                    <li className="navItem">
-                        <Link href="/register">Registrar</Link>
-                    </li>
-                    <li className="navItem">
-                        <Link href="/login">Login</Link>
-                    </li>
-                </ul>
-            </nav>
+        <div className={styles.bloco}>
+            <div className={styles.background}>
+                <LoginCard title="unbOok">
+                    <p>É ótimo tê-lo por aqui!</p>
+                    <Button><Link href="/login">Login</Link></Button>
+                    <Button><Link href="/register">Cadastro</Link></Button>
+                </LoginCard>
+            </div>
         </div>
     );
 }
