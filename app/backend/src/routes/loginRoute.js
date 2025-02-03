b4292463c1f9e9148c7229e732908a5762038054
SAnjos3 <academicanjos@gmail.com>
2025-02-03 19:08:20 -0300

    [feat](login routes): adiconando uma nova rota de status, para verificar a sessão de usuário

--- app/backend/src/routes/loginRoute.js
+++ app/backend/src/routes/loginRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { login, logout } from '../controllers/authController.js';
 import { checkNotAuth } from '../middlewares/auth.js';
-
+import { debugSession } from '../middlewares/auth.js';
 const router = express.Router();
 
+router.get("/status", debugSession);
 router.post('/login', checkNotAuth, login);
 router.delete('/logout', logout);
 
