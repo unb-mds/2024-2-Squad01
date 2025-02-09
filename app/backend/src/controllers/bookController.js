a3df26a0153ad83508e83ad7d5d14da612c5eb2b
SAnjos3 <academicanjos@gmail.com>
2025-02-09 18:16:17 -0300

    [Feat&Fix](BookRoutes): Feitas correçoes necessarias no componente modal para o devido registro dos livros. Alem disso, foram feitos ajustes na função de criaçao de livros no backend, e adicionado uma gerencia de dados middleware para lidar com atributos do tipo imagem.

--- app/backend/src/controllers/bookController.js
+++ app/backend/src/controllers/bookController.js
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client"
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export const createBook = async (req, res) => {
     try {
@@ -10,25 +10,24 @@ export const createBook = async (req, res) => {
                 descricao: req.body.descricao,
                 status: "Ativo",
                 objetivo: req.body.objetivo,
-                foto: req.body.foto,
+                foto: req.file ? req.file.filename : null,
                 data_de_publicacao: new Date(),
                 email_publicador: req.user.email
             }
-        })
+        });
         res.status(201).json({
             message: "Livro criado com sucesso",
             book: newBook
-        })
+        });
     } catch (error) {
         console.error('Erro ao criar publicação do livro:', error);
         res.status(400).json({ error: 'Erro ao criar publicação de Livro' });
     }
-}
+};
+
 export const getBooks = async (req, res) => {
-    console.log("chamou função")
     try {
         const books = await prisma.book.findMany();
-        console.log("Livros enconotrados: ", books)
         return res.json(books);
     } catch (error) {
         console.error("Erro ao buscar livros:", error);
--- app/backend/src/routes/bookRoute.js
+++ app/backend/src/routes/bookRoute.js
@@ -1,12 +1,22 @@
 import express from 'express';
-import { checkAuth } from '../middlewares/auth.js'
-import { createBook, getBooks } from '../controllers/bookController.js'
+import { checkAuth } from '../middlewares/auth.js';
+import multer from 'multer';
+import { createBook, getBooks } from '../controllers/bookController.js';
 import { debugSession } from '../middlewares/auth.js';
 
 const router = express.Router();
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+const upload = multer({ storage });
 
 router.use(debugSession);
-router.post('/createBook', checkAuth, createBook)
+router.post('/createBook', checkAuth, upload.single('foto'), createBook);
 router.get('/', getBooks)
 
 export default router
