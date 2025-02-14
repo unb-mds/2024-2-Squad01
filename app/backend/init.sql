e167183f2f2614cd3c1a82c1b4f7d7779d43fc83
SAnjos3 <academicanjos@gmail.com>
2025-02-14 05:47:05 -0300

    [feat](docker-compose.yml&init.sql): Alterando o arquivo docker-compose.yml para aplicar o script init.sql durante a criação do docker.

--- app/backend/init.sql
+++ app/backend/init.sql
@@ -1,4 +1,9 @@
-DROP USER IF EXISTS 'team_user'@'%';
-CREATE USER 'team_user'@'%' IDENTIFIED BY 'team_pass';
-GRANT ALL PRIVILEGES ON *.* TO 'team_user'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'team_user') THEN
+        CREATE USER team_user WITH PASSWORD 'team_pass';
+    END IF;
+END$$;
+
+GRANT ALL PRIVILEGES ON DATABASE unbookinho TO team_user;
+CREATE DATABASE unbookinho_shadow;
