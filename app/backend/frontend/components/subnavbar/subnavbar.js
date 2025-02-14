9572406317234c4b4c8828bfebc9e3a390c58a54
mymendonca <mylenamendonca6@gmail.com>
2025-02-14 00:18:01 -0300

    "[STYLE](sobre nos) Continuação da página sobre nos (colocando imagens)"
    Co-authored-by: joaofmoreiraa<joaofmoreiraa@users.noreply.github.com>
    Co-authored-by: mclarasena <mclarasena@users.noreply.github.com>"

--- app/backend/frontend/components/subnavbar/subnavbar.js
+++ app/backend/frontend/components/subnavbar/subnavbar.js
@@ -7,7 +7,7 @@ const SubNavbar = () => {
         <nav className={styles.navbar}>
             <ul className={styles.navLinks}>
                 <li className={styles.botoes}><Link href="/feed">Feed</Link></li>
-                <li className={styles.botoes}><Link href="/sobrenos">Sobre Nós</Link></li>
+                <li className={styles.botoes}><Link href="/sobrenos">Sobre Nos</Link></li>
                 
             </ul>
         </nav>
